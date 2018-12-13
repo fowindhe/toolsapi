f6f51c151183d4ac849c661c9aec479cadc7ef4d
Eason <fowindhe@163.com>
2018-12-13 17:59:54 +0800

    Update common.cpp

--- common.cpp
+++ common.cpp
@@ -80,3 +80,108 @@ void FormateSrcStr(string& strSrc)
     return;
 }
 
+/*************************************************
+* 函数介绍：获取下个关键字值，并将源串第一个关键字值去掉
+* 输入参数：源字符串，关键字参数
+* 输出参数：源字符串，关键字值
+* 返回值  ：错误号
+*************************************************/
+void GetNextValue(string &src,string &value)
+{
+    int ipos = 0;
+    if(src.length() < 1)
+    {
+        return;
+    }
+
+    ipos = src.find_first_of(' ');
+    if(ipos < 0)
+    {
+        return;
+    }
+    src = src.substr(ipos);
+
+    ipos = src.find_first_not_of(' ');
+    if(ipos < 0)
+    {
+        return;
+    }
+    src = src.substr(ipos);
+
+    ipos = src.find_first_of(' ');
+    if(ipos < 0)
+    {
+        value = src;
+    }
+    else
+    { 
+        value = src.substr(0,ipos);
+    }
+}
+
+
+/*************************************************
+* 函数介绍：主机信息 主机名IP
+* 输入参数：无
+* 输出参数：无
+* 返回值  ：错误号
+*************************************************/
+int GetHostInfo()
+{
+    int ret                             = ERR_OK;
+    FILE *fp                            = NULL;
+    char commandbuf[MAXCOMMANDLEN]      = {0};
+    char infolinebuf[MAXINFOLINELEN]    = {0};
+    int len                             = 0;
+    char tmpHostName[256]               = {0};
+    string tmpIP                        = "";
+
+	ret = gethostname(tmpHostName, sizeof(tmpHostName));
+    if(ret != 0)
+    {
+        TsgErrorLog("Secchk gethostname failed ");
+        return ERR_GETHOST_FAILED;
+    }
+
+    len = snprintf(commandbuf, MAXCOMMANDLEN, "ifconfig | grep 'inet addr:'| grep 'Bcast:' | cut -f 2 -d :|cut -f 1 -d ' '");
+
+    if (len < 0)
+    {
+        TsgErrorLog("snprintf getip cmd failed ");
+        return ERR_SNPRINT_FAILED;
+    }
+
+    fp = popen(commandbuf, "r");
+
+    if (fp == NULL)
+    {
+        TsgErrorLog("popen getip cmd failed");
+        return ERR_OPEN_FAILED;
+    }
+
+    while (fgets(infolinebuf, MAXINFOLINELEN, fp))
+    {
+        if (strlen(infolinebuf) < 1)
+        {
+            continue;
+        }
+        
+        tmpIP = infolinebuf;
+        if (tmpIP.substr(tmpIP.length() - 1) == "\n")
+        {
+            tmpIP = tmpIP.substr(0, tmpIP.length() - 1);
+        }
+        break;
+
+    }
+
+    m_hostInfo->hostname = tmpHostName;
+    m_hostInfo->ip = tmpIP;
+
+    pclose(fp);
+    fp = NULL;
+    return ret;
+}
+
+
+
